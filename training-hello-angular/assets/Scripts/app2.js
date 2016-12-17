3fedfd5713f0d45dff6686e0c2333d8207bdea9d
wesley_henrique_123@hotmail.com <wesley_henrique_123@hotmail.com>
2016-12-17 00:47:46 -0200

    Angular

---
+++ training-hello-angular/assets/Scripts/app2.js
@@ -0,0 +1,33 @@
+
+(function () {
+    angular.module('fasebook',[]);
+    angular
+        .module('fasebook')
+        .controller('controlador',function ($scope) {
+            $scope.postagens = [{
+                autor: 'Wesley',
+                status: 'online',
+                texto: 'Não achei o botão de sair, vou ficar online só mais um pouquinho kkkkkkk',
+                curtir: false,
+                tempo: 'Postado há 11 minutos',
+
+
+            },
+
+            $scope.postagens={
+                autor: 'Wesley',
+                status: 'online',
+                texto: 'Não achei o botão de sair, vou ficar online só mais um pouquinho kkkkkkk',
+                curtir: false,
+                tempo: 'Postado há 11 minutos'
+
+        }
+        ]
+
+
+
+
+            });
+
+
+})()
---
+++ training-hello-angular/assets/Scripts/simpleControler.js
@@ -0,0 +1,9 @@
+(function () {
+
+
+var app = angular.module('app', []);
+app.controller('simpleController', function ($scope) {
+    $scope.user = {name:"wesley"}
+});
+
+})()
